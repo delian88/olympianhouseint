e7b9ab13a37a1e8803ddecf38f1d2b827086bbbb
Taiwo Oladele <taiwodele88@gmail.com>
2026-08-31 21:31:54 +0100

    chore: remove ValueProposition section completely

--- src/pages/LandingPage/Home.jsx
+++ src/pages/LandingPage/Home.jsx
@@ -8,7 +8,6 @@ import { Badge } from "../../components/ui/badge";
 import Reveal from "../../components/ui/reveal";
 import { Marquee } from "../../components/LandingPage/magicui/marquee";
 import UnderlinedHeading from "../../components/LandingPage/UnderlinedHeading";
-import ValueProposition from "../../components/LandingPage/ValueProposition/ValueProposition";
 
 
 function FallbackImage({ src, fallback, alt, className, ...props }) {
@@ -463,16 +462,6 @@ function Home() {
           </div>
         </div>
       </section>
-
-      <ValueProposition />
-
-
-
-      
-
-
-
-      
 
       
 
